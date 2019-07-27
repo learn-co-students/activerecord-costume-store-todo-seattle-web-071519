8cfd5df2dfb038bb6911990c6a26c2e7d8ee3ece
marshallhelenm <marshallhelenm@gmail.com>
2019-07-27 12:23:01 -0700

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,28 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.string :location
+            t.string :theme
+            t.integer :price
+            t.boolean :family_friendly
+            t.datetime :opening_date
+            t.datetime :closing_date
+            t.string :description
+            t.datetime :created_at
+            t.datetime :updated_at
+        end
+    end
+end
+
+
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
